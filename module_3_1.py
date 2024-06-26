ccb1464fb6411e35d33c1eeb65b15916bc4e4e2c
Natalya Savchenko <natasolod@icloud.com>
2024-06-26 11:01:49 +0500

    Create module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+  global calls
+  calls += 1
+  return calls
+
+def string_info(string):
+  b = ()
+  b = (len(string), string.upper(), string.lower())
+  count_calls()
+  return (b)
+
+def is_contains(string, list_to_search):
+  count_calls()
+
+  for i in list_to_search:
+    if i.upper() in string.upper():
+      return True
+    else:
+      return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
